2e06b836862007779f623fd06ffcbc56eff2f678
Carlos Galvez <carlosgalvezp@gmail.com>
2017-09-26 18:57:56 +0200

    Fix pylint warning

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -8,7 +8,7 @@ class Controller(object):
     def __init__(self, vehicle_mass, decel_limit, accel_limit, wheel_radius,
                  wheel_base, steer_ratio, max_lat_accel, max_steer_angle,
                  brake_deadband, fuel_capacity):
-        self.velocity_pid = PID(1.5, 0.001, 0.0, mn=decel_limit, mx=accel_limit)
+        self.velocity_pid = PID(1.5, 0.001, 0., mn=decel_limit, mx=accel_limit)
         self.yaw_controller = YawController(wheel_base, steer_ratio, 5,
                                             max_lat_accel, max_steer_angle)
         self.wheel_radius = wheel_radius
