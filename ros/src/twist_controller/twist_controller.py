4fcd47996f4cd3b4e3d2df3dd41b9617a6692237
Carlos Galvez <carlosgalvezp@gmail.com>
2017-09-26 18:07:52 +0200

    Remove D component of PID for smooth acceleration and braking

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,3 +1,4 @@
+import rospy
 from pid import PID
 from yaw_controller import YawController
 GAS_DENSITY = 2.858
@@ -8,7 +9,7 @@ class Controller(object):
     def __init__(self, vehicle_mass, decel_limit, accel_limit, wheel_radius,
                  wheel_base, steer_ratio, max_lat_accel, max_steer_angle,
                  brake_deadband, fuel_capacity):
-        self.velocity_pid = PID(0.2, 0.0, 3, mn=decel_limit, mx=accel_limit)
+        self.velocity_pid = PID(0.2, 0.001, 0.0, mn=decel_limit, mx=accel_limit)
         self.yaw_controller = YawController(wheel_base, steer_ratio, 5,
                                             max_lat_accel, max_steer_angle)
         self.wheel_radius = wheel_radius
@@ -24,6 +25,11 @@ class Controller(object):
         steer = self.yaw_controller.get_steering(twist.twist.linear.x,
                                                  twist.twist.angular.z,
                                                  velocity.twist.linear.x)
+
+        rospy.logwarn('Current V: {}; Target V: {}; Acceleration CMD: {}'
+                      .format(velocity.twist.linear.x,
+                              twist.twist.linear.x,
+                              linear_acceleration))
 
         if linear_acceleration > 0.0:
             throttle = linear_acceleration
