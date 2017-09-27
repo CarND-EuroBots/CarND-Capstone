3aa8be9daa657ba39fd9ad76b396e37ef65bc3eb
Carlos Galvez <carlosgalvezp@gmail.com>
2017-09-27 21:22:29 +0200

    Apply full brake if target velocity is close to 0
    This avoids jerk when car is standing by, waiting for the lights
    to go green.
    
    Fixes #106

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -2,7 +2,7 @@ import rospy
 from pid import PID
 from yaw_controller import YawController
 GAS_DENSITY = 2.858
-
+FULL_BRAKE_SPEED = 0.1  # If target velocity is smaller, apply full brake
 
 class Controller(object):
     def __init__(self, vehicle_mass, decel_limit, accel_limit, wheel_radius,
@@ -14,6 +14,7 @@ class Controller(object):
         self.wheel_radius = wheel_radius
         self.brake_deadband = brake_deadband
         self.total_mass = vehicle_mass + fuel_capacity * GAS_DENSITY
+        self.decel_limit = decel_limit
 
     def reset(self):
         self.velocity_pid.reset()
@@ -25,18 +26,31 @@ class Controller(object):
                                                  twist.twist.angular.z,
                                                  velocity.twist.linear.x)
 
-        if linear_acceleration > 0.0:
-            throttle = linear_acceleration
-            brake_torque = 0.0
-        else:
+        # Keep full brake if target velocity is almost 0
+        if twist.twist.linear.x < FULL_BRAKE_SPEED:
             throttle = 0.0
-            deceleration = -linear_acceleration
+            brake_torque = self.acceleration_to_torque(abs(self.decel_limit))
+        else:
+            if linear_acceleration > 0.0:
+                throttle = linear_acceleration
+                brake_torque = 0.0
+            else:
+                throttle = 0.0
+                deceleration = -linear_acceleration
 
-            # Do not brake if too small deceleration
-            if deceleration < self.brake_deadband:
-                deceleration = 0.0
+                # Do not brake if too small deceleration
+                if deceleration < self.brake_deadband:
+                    deceleration = 0.0
 
-            # Compute brake torque, in Nm
-            brake_torque = deceleration * self.total_mass * self.wheel_radius
+                # Compute brake torque, in Nm
+                brake_torque = self.acceleration_to_torque(deceleration)
 
         return throttle, brake_torque, steer
+
+    def acceleration_to_torque(self, acceleration):
+        """
+        Transforms acceleration to torque
+        Input: acceleration (float) acceleration, in m/s^2
+        Output: torque (float) torque, in Nm
+        """
+        return acceleration * self.total_mass * self.wheel_radius
