3b0fd9b0e59613615afe17a01f7b97e1337a129e
Ivan Danov <idanov@users.noreply.github.com>
2017-09-30 19:43:01 +0100

    Add a low pass filter for the linear acceleration

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,6 +1,7 @@
 import rospy
 from pid import PID
 from yaw_controller import YawController
+from lowpass import LowPassFilter
 GAS_DENSITY = 2.858
 FULL_BRAKE_SPEED = 0.1  # If target velocity is smaller, apply full brake
 
@@ -9,6 +10,7 @@ class Controller(object):
     def __init__(self, vehicle_mass, decel_limit, accel_limit, wheel_radius,
                  wheel_base, steer_ratio, max_lat_accel, max_steer_angle,
                  brake_deadband, fuel_capacity, max_throttle_percent):
+        self.acc_filter = LowPassFilter(4., 1.)
         self.velocity_pid = PID(1.5, 0.001, 0.,
                                 mn=decel_limit, mx=max_throttle_percent)
         self.yaw_controller = YawController(wheel_base, steer_ratio, 1,
@@ -28,6 +30,8 @@ class Controller(object):
                                                  twist.twist.angular.z,
                                                  velocity.twist.linear.x)
 
+        # Apply low-pass filter to the linear acceleration
+        linear_acceleration = self.acc_filter.filt(linear_acceleration)
         # Keep full brake if target velocity is almost 0
         if twist.twist.linear.x < FULL_BRAKE_SPEED:
             throttle = 0.0
